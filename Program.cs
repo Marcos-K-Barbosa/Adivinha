b700504f83ebd70b90b97889b92b0f75ec373176
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-14 20:29:42 -0300

    Programa de Adivinhação Concluído.

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Adivinhe.");
+Console.WriteLine("Oque é. . . Oque é. . .");
+Console.WriteLine(". . . Todos já possuiram por princípio, mas todos querem ter no futuro. . .");
+Thread.Sleep(5000);
+Console.WriteLine("A Juventude.");
